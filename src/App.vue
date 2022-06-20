75b4c0b254138d47fd63b6c7833202dac6d555be
Mouse <miss.mousewife@gmail.com>
2022-06-19 20:42:55 -0500

    add pilots

--- src/App.vue
+++ src/App.vue
@@ -55,10 +55,22 @@ export default {
           "age": "36u",
           "pob": "[CLASSIFIED]",
         },
-        "RAI-DEN": {
+        "DWARF": {
           "age": "48u",
           "pob": "Gaia"
-        }
+        },
+		"RHAPSODY": {
+			"age": "??"
+			"pob": "??"
+		},
+		"SHRIKE": {
+			"age": "??"
+			"pob": "??"
+		},
+		"MIRACLE": {
+			"age": "??"
+			"pob": "??"
+		}
       }
     };
   },
