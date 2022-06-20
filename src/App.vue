16d9d700ca091f07c4547dc63aac016a60776733
Mouse <miss.mousewife@gmail.com>
2022-06-19 20:22:03 -0500

    large update, pilots to come

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,15 @@
 <template>
   <Header :header="this.header" />
-  <div class="content-container">
-    <MissionView
-      :missions="this.missions"
-      :missionMarkdown="this.current_md"
-      :selected="this.mission_slug"
-      v-on:missionSelected="handleMissionSelected"
-    />
-
-    <EventsView :events="this.events" />
-    <section class="section-container" id="main-tab">
-      <div class="main-tab-header" style="height:52px; overflow:hidden;">
-        <div class="section-header clipped-medium-backward-pilot">
-          <img :src="mainTabIcon" />
-          <h1>{{ mainTabTitle }}</h1>
-        </div>
-        <TabButton
-          v-for="item in this.options.panelOptions"
-          :name="item"
-          :hidden="item === this.options.mainPanel"
-          :key="item"
-          @click="selectMainPanel(item)"
-        />
-        <div class="rhombus-back">&nbsp;</div>
-      </div>
-      <div class="section-content-container">
-        <transition name="fade" mode="out-in">
-          <PilotsView v-if="this.options.mainPanel === 'pilot'" :pilots="this.pilots" />
-          <NPCView v-else-if="this.options.mainPanel === 'npc'" :npcs="this.npcs" />
-          <GlossaryView v-else-if="this.options.mainPanel === 'glossary'" />
-          <ClocksView v-else-if="this.options.mainPanel === 'clock'" :clocks="this.clocks" />
-        </transition>
-      </div>
-    </section>
+  <Sidebar />
+  <div id="router-view-container">
+    <router-view :missions="missions" :events="events" :pilots="pilots" :clocks="clocks" :reserves="reserves" />
   </div>
-  <svg
-    style="visibility: hidden; position: absolute;"
-    width="0"
-    height="0"
-    xmlns="http://www.w3.org/2000/svg"
-    version="1.1"
-  >
+  <svg style="visibility: hidden; position: absolute" width="0" height="0" xmlns="http://www.w3.org/2000/svg"
+    version="1.1">
     <defs>
       <filter id="round">
         <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
-        <feColorMatrix
-          in="blur"
-          mode="matrix"
-          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
-          result="goo"
-        />
+        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5" result="goo" />
         <feComposite in="SourceGraphic" in2="goo" operator="atop" />
       </filter>
     </defs>
@@ -61,250 +21,141 @@
 </template>
 
 <script>
-import Header from './components/layout/Header.vue';
-import Footer from './components/layout/Footer.vue';
-import MissionView from './components/layout/MissionView.vue';
-import EventsView from './components/layout/EventsView.vue';
-import PilotsView from './components/layout/PilotsView.vue';
-import NPCView from './components/layout/NPCView.vue';
-import ClocksView from './components/layout/ClocksView.vue';
-import GlossaryView from './components/layout/GlossaryView.vue';
-import TabButton from './components/TabButton.vue'
+import Header from "./components/layout/Header.vue";
+import Sidebar from "./components/layout/Sidebar.vue";
+import Footer from "./components/layout/Footer.vue";
 
 export default {
   components: {
     Header,
     Footer,
-    MissionView,
-    EventsView,
-    PilotsView,
-    NPCView,
-    GlossaryView,
-    ClocksView,
-    TabButton,
+    Sidebar,
   },
 
   data() {
     return {
-      "mission_slug": "006",
-      "current_md": "",
-      "events": "",
-      "missions": [
-        {
-          "slug": "001",
-          "name": "Bug-Hunt",
-          "status": "success"
-        },
-		{
-          "slug": "002",
-          "name": "Vigilant Gaze",
-          "status": "partial-success"
-        },
-		{
-          "slug": "003",
-          "name": "Floodgate",
-          "status": "success"
-        },
-		{
-          "slug": "004",
-          "name": "Rallying Cry",
-          "status": "success"
-        },
-		{
-          "slug": "005",
-          "name": "Split-Knuckle Haymaker",
-          "status": "success"
-        },
-		{
-			"slug": "006",
-			"name": "Dawn of a New War",
-			"status": "start"
-		}
-      ],
-      "pilots": [
-        {
-          "callsign": "Dwarf",
-          "alias": "Arthur Fredrick Bradshaw",
-          "code": "Bradshaw.Arthur.Fredrick:5515314a-b2e8-4717-a7ef-853bf036de35//NDL-C-BARYON-TEMPLE",
-          "corpro": "IPS-N",
-          "frame": "Blackbeard",
-          "mech": "Atgeir"
-        },
-        {
-          "callsign": "Miracle",
-          "alias": "Miroslava Vlablinskaja",
-          "code": "Vlablinskaja.Miroslava:587f9a36-bf6b-4505-bc21-1b88e18a144d//NDL-C-SINGULARITY-DECEMBER",
-          "corpro": "SSC",
-          "frame": "Death's Head",
-          "mech": "Angrboða"
-        },
-        {
-          "callsign": "Eclipse",
-          "alias": "ᏃᏈᏏ ᎤᏕᏅ",
-          "code": "ᎤᏕᏅ.Cat.Noquisi.Udenv./.ᏃᏈᏏ:602cf4a4-58a4-4c42-a736-8f64c7b0b75a//NDL-C-CHAIN-SKULL",
-          "corpro": "HORUS",
-          "frame": "Balor",
-          "mech": "Phobos"
-        },
-        {
-          "callsign": "Shrike",
-          "alias": "Sokolova",
-          "code": "Sokolova:9b84f5f3-3a22-44c9-853d-c223fe3dca03//NDL-C-BRONZE-SWORD",
-          "corpro": "IPS-N",
-          "frame": "Raleigh",
-          "mech": "Creosote"
-        },
-        {
-          "callsign": "Rhapsody",
-          "alias": "Caelia Pagett",
-          "code": "Pagett.Caelia:a222e882-b71c-418c-ad8a-2b0dcb7b5b5d//NDL-C-LUNAR-DREAM",
-          "corpro": "SSC",
-          "frame": "Dusk Wing",
-          "mech": "Staccato"
-        },
-      ],
-      "npcs": [
-        {
-          "name": "Brava Hadura",
-          "affiliation": "Evergreen",
-          "pronouns": "She/Her",
-          "notes": "Militia Commander"
-        },
-		{
-          "name": "Castor Fielding",
-          "affiliation": "Evergreen",
-          "pronouns": "He/Him",
-          "notes": "Chief Engineer"
-        },
-		{
-          "name": "Dthall Ordo",
-          "affiliation": "Hercynian United Cities",
-          "pronouns": "She/Her",
-          "notes": "HUC Ranger Liaison"
-        },
-		{
-          "name": "Endeavor",
-          "affiliation": "Hercynian United Cities",
-          "pronouns": "They/Them",
-          "notes": "Egregorian Overmind"
-        },
-		{
-          "name": "Ilyr Ordo",
-          "affiliation": "Hercynian United Cities",
-          "pronouns": "He/Him",
-          "notes": "Hivehome Commander"
-        },
-		{
-          "name": "Primoz Commorand",
-          "affiliation": "Hercynian United Cities",
-          "pronouns": "He/Him",
-          "notes": "Daylight Commnader"
-        },
-		{
-          "name": "Pyotr Heidel",
-          "affiliation": "Hercynian United Cities",
-          "pronouns": "He/Him",
-          "notes": "Mycol Fields Commander"
-        },
-		{
-          "name": "Terror",
-          "affiliation": "Hercynian United Cities",
-          "pronouns": "They/Them",
-          "notes": "HUC Ranger Commander"
-        }
-      ],
-      "header": {
-        "planet": "Hercynia",
-        "year": "5014u",
-        "system": "Ardennes-3",
-        "gate": "Atlas-Quanokrim",
-        "ring": "Atlas-Line",
-        "headerTitle": "Hercynian",
-        "headerSubtitle": "United Cities",
-        "subheaderTitle": "Rangers",
-        "subheaderSubtitle": "382nd MECH",
+      header: {
+        planet: "Hercynia",
+        year: "5014u",
+        system: "Ardennes-3",
+        gate: "Atlas-Quanokrim",
+        ring: "Atlas-Line",
+        headerTitle: "Hercynian",
+        headerSubtitle: "United Cities",
+        subheaderTitle: "Rangers",
+        subheaderSubtitle: "382nd Mechanized (Provisional)",
       },
-      "clocks": [
-      ],
-      "options": {
-        "eventsMarkdownPerMission": true,
-        "mainPanel": "pilot",
-        "panelOptions": [
-          "pilot",
-          "npc",
-          "glossary",
-          "clock"
-        ]
+      clocks: [],
+      events: [],
+      missions: [],
+      pilots: [],
+      reserves: [],
+      init: {
+        "APOLLO": {
+          "age": "32u",
+          "pob": "Evergreen",
+        },
+        "RAI-DEN": {
+          "age": "48u",
+          "pob": "Gaia"
+        }
       }
-    }
+    };
   },
-
-  created() {
-    this.loadMissionMarkdown()
-    this.loadEventsMarkdown()
+  created() { },
+  mounted() {
+    this.importMissions(import.meta.glob("@/assets/missions/*.md", { as: "raw" }));
+    this.importEvents(import.meta.glob("@/assets/events/*.md", { as: "raw" }));
+    this.importClocks(import.meta.glob("@/assets/clocks/*.json"));
+    this.importReserves(import.meta.glob("@/assets/reserves/*.json"));
+    this.importPilots(import.meta.glob("@/assets/pilots/*.json"));
+    this.$router.push("/status");
   },
-
-  computed: {
-    mainTabTitle() {
-      if (this.options.mainPanel === "pilot") return "Pilot Roster"
-      if (this.options.mainPanel === "npc") return "Persons Registry"
-      if (this.options.mainPanel === "glossary") return "Lexicon"
-      if (this.options.mainPanel === "clock") return "Clocks"
-    },
-    mainTabIcon() {
-      return `/icons/${this.options.mainPanel}-icon.svg`
-    }
-  },
-
   methods: {
-    handleMissionSelected(mission) {
-      this.mission_slug = mission.slug;
-      this.loadMissionMarkdown()
-      if (this.options.eventsMarkdownPerMission) {
-        this.loadEventsMarkdown();
+    importMissions(files) {
+      for (const path in files) {
+        let mission = {};
+        const content = files[path];
+        mission["slug"] = content.split("\n")[0];
+        mission["name"] = content.split("\n")[1];
+        mission["status"] = content.split("\n")[2];
+        mission["content"] = content.split("\n").splice(3).join("\n");
+        this.missions.push(mission);
       }
     },
-    loadMissionMarkdown() {
-      let self = this;
-      let md = `/missions/${self.mission_slug}.md`
-      var client = new XMLHttpRequest();
-      client.open('GET', md);
-      client.onreadystatechange = function () {
-        self.current_md = client.responseText;
+    importEvents(files) {
+      for (const path in files) {
+        let event = {};
+        const content = files[path];
+        event["title"] = content.split("\n")[0];
+        event["location"] = content.split("\n")[1];
+        event["time"] = content.split("\n")[2];
+        event["thumbnail"] = content.split("\n")[3];
+        event["content"] = content.split("\n").splice(4).join("\n");
+        this.events.push(event);
       }
-      client.send();
     },
-    loadEventsMarkdown() {
-      let self = this;
-      let md = "";
+    importClocks(files) {
+      for (const path in files) {
+        files[path]().then((mod) => {
+          this.clocks = JSON.parse(JSON.stringify(mod)).default;
+        });
+      }
+    },
+    importReserves(files) {
+      for (const path in files) {
+        files[path]().then((mod) => {
+          this.reserves = JSON.parse(JSON.stringify(mod)).default;
+        });
+      }
+    },
+    importPilots(files) {
+      for (const path in files) {
+        files[path]().then((mod) => {
+          var pilotFromJson = JSON.parse(JSON.stringify(mod));
+          var pilotFromVue = this.init[pilotFromJson.callsign.toUpperCase()];
+          var pilot = {
+            ...pilotFromJson,
+            ...pilotFromVue
+          }
+          this.pilots.push(pilot);
+          
+          pilot.clocks.forEach((clock) => {
+            this.clocks.push({
+              type: `Pilot Project // ${pilot.callsign}`,
+              result: "",
+              name: clock.title,
+              description: clock.description,
+              value: clock.progress,
+              max: clock.segments,
+              color: "#3CB043"
+            })
+          })
 
-      if (self.options.eventsMarkdownPerMission) {
-        md = `/events/${self.mission_slug}.md`
+          pilot.reserves.forEach((reserve) => {
+            this.reserves.push({
+              type: reserve.type,
+              name: reserve.name,
+              description: reserve.description,
+              label: reserve.label,
+              cost: reserve.cost,
+              notes: reserve.notes,
+              callsign: pilot.callsign.toUpperCase()
+            })
+          })
+        });
       }
-      else {
-        md = "/events.md"
-      }
-
-      var client = new XMLHttpRequest();
-      client.open('GET', md);
-      client.onreadystatechange = function () {
-        self.events = client.responseText;
-      }
-      client.send();
     },
-    selectMainPanel(panel) {
-      this.options.mainPanel = panel;
-    }
   }
-
-}
+};
 </script>
 
-
-<style lang="scss">
+<style>
 #app {
-  width: 1902px;
-  height: 910px;
-  overflow: hidden;
+  width: 1901px;
+  height: 924px;
+  overflow: hidden !important;
+  border-right: 1px solid #ff0;
+  border-bottom: 1px solid #ff0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import Oruga from '@oruga-ui/oruga-next'
-import '@oruga-ui/oruga-next/dist/oruga.min.css'
+import Oruga from "@oruga-ui/oruga-next";
+import "@oruga-ui/oruga-next/dist/oruga.min.css";
 
-import '@/assets/styles/_reset.css'
-import '@/assets/styles/_base.css'
-import '@/assets/styles/_animations.css'
+import "@mdi/font/css/materialdesignicons.css";
+
+import "@/assets/styles/_reset.css";
+import "@/assets/styles/_base.css";
+// import '@/assets/styles/_animations.css'
+
+import Router from "./router";
+import VueWriter from "vue-writer";
 
 createApp(App)
+	.use(Router)
 	.use(Oruga)
-	.mount('#app')
+	.use(VueWriter)
+	.mount("#app");
